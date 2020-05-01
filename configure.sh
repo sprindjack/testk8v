27ce5a7d487fa67f5906e7ebe0842b11f480a528
sprindjack <sprindjack@hotmail.com>
2020-05-02 00:59:48 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
 # V2Ray new configuration
 cat <<-EOF > /etc/v2ray/config.json
-$CONFIG_JSON
+{
+    "inbounds": [
+        {
+            "port": 80,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "8feb2bc6-fdfe-4e50-b9b6-08f87f69dcf6",
+                        "alterId": 64
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "wsSettings":{
+                    "path": "/pic"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
 EOF
 # Run V2Ray
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
